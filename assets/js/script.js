a857cab790c82b9c24f3eb066e341ed451d83c99
Deiontre10 <travontazlowry@gmail.com>
2022-12-08 17:35:25 -0500

    added comments to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 
 $(function () {
+  // Declare global variables
   var date = dayjs();
   var currentHour = dayjs().hour();
   var saveButton = $(".saveBtn")
   var hour = $(".time")
 
+  // Adds the current date and time in the header
   $("#currentDay").text(date.format("dddd, MMMM DD | HH:mm:ss A"))
  
+  // Iterate through each time class starting from index 0
   hour.each(function () {
     var time = parseInt($(this).attr("id"))
     if (time < currentHour) {
@@ -19,6 +22,7 @@ $(function () {
     
   });
   
+  // Adds a click event to the save button that updates local storage
   saveButton.on("click", function(){
     var buttonClick = $(this).parent().attr("id");
     var textInput = $(this).siblings(".description").val();
